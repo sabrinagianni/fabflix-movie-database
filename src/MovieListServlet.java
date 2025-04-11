f0c779693c9741ac4e4ba134a2150e0362c72f11
Sabrina Pukarta <sabrinagiannii@gmail.com>
2025-04-10 23:45:25 -0700

    Implemented MovieListServlet. Added SingleMovieServlet and SingleStarServlet

---
+++ src/MovieListServlet.java
@@ -0,0 +1,91 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.httpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet(name = "MovieListServlet", urlPatterns = "/api/movielist")
+public class MovieListServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // check web.xml
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        }
+        catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+
+        //String id = request.getParameter("id");
+
+        //request.getServletContext().log("getting id: " + id);
+
+        PrintWriter out = response.getWriter();
+
+        try (Connection conn = dataSource.getConnection()) {
+            String query = "SELECT m.id, m.title, m.year, m.director, r.rating" +
+                            "FROM movies m, ratings r" +
+                            "WHERE m.id = r.movieId" +
+                            "ORDER BY r.rating DESC LIMIT 20";
+
+            PreparedStatement statement = conn.prepareStatement(query);
+
+            //statement.setString(1, id);
+
+            ResultSet rs = statement.executeQuery();
+
+            JsonArray jsonArray = new JsonArray();
+
+            while(rs.next()) {
+                String id = rs.getString("id");
+                String title = rs.getString("title");
+                String year = rs.getString("year");
+                String director = rs.getString("director");
+                String rating = rs.getString("rating");
+
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("id", id);
+                jsonObject.addProperty("title", title);
+                jsonObject.addProperty("year", year);
+                jsonObject.addProperty("director", director);
+                jsonObject.addProperty("rating", rating);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+
+            out.write(jsonArray.toString());
+
+            response.setStatus(200);
+        } catch (Exception e) {
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+    }
+}
