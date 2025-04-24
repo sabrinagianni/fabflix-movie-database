e6ebbcdb9a58c46d901fdcc13d54daefe9f3a470
Julia Tjia <tjiaja@uci.edu>
2025-04-24 02:29:51 -0700

    implemented login page

---
+++ WebContent/js/login.js
@@ -0,0 +1,30 @@
+let login_form = $("#login_form");
+
+function handleLoginResult(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
+
+
+    console.log("handle login response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["status"]);
+
+    if (resultDataJson["status"] === "success") {
+        window.location.replace("movielist.html");
+    } else {
+        console.log("show error message");
+        console.log(resultDataJson["message"]);
+        $("#login_error_message").text(resultDataJson["message"]);
+    }
+}
+
+function submitLoginForm(event) {
+    event.preventDefault();
+
+    $.ajax("api/login", {
+        method: "POST",
+        data: login_form.serialize(),
+        success: handleLoginResult
+    });
+}
+
+login_form.submit(submitLoginForm);
