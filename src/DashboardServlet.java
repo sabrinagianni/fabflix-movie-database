b0c185585dfbf67bc94c4f58019d6204902ce022
Sabrina Pukarta <sabrinagiannii@gmail.com>
2025-05-12 14:20:32 -0700

    Dashboard Servlet

---
+++ src/DashboardServlet.java
@@ -0,0 +1,19 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "DashboardServlet", urlPatterns = "/_dashboard")
+public class DashboardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        if (request.getSession().getAttribute("employee") == null) {
+            response.sendRedirect("_dashboard.html");
+        } else {
+            response.sendRedirect("_dashboard_main.html");
+        }
+    }
+}
